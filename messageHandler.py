f40d46cdc3bb3a0264687b05c059b1fdde6b6650
igouProto <igouProto@users.noreply.github.com>
2019-12-26 23:15:08 +0800

    The message handler responsible for replying to keywords and the "Talk through portal" function

---
+++ messageHandler.py
@@ -0,0 +1,44 @@
+#discord py modules
+import discord
+from discord.ext import commands
+import asyncio
+
+class messageHandler(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.keywords = []
+		self.response = {}
+		###read the keyword pairs from file###
+		with open('keywords.txt', 'r') as data:
+			for line in data:
+				(kw, resp) = line.split(':', 1)
+				self.response[kw] = str(resp)
+			for (kw, resp) in self.response.items():
+				self.keywords.append(kw)
+		data.close()
+
+	@commands.Cog.listener()
+	async def on_message(self, message):
+	###Keyword respond function###
+		if message.content in self.keywords:
+			if message.author == self.bot.user:
+				return
+			else:
+				print ('keyword hit')
+				msg = self.response[message.content]
+				await message.channel.send(msg)
+		###portal talking function
+		if message.channel.id == (474980362263724032):  #the portal
+			print(self.messagedest)		
+			await self.bot.get_channel(self.messagedest).send(message.content)
+
+	@classmethod
+	def set_destination(self, channel_id):
+		self.messagedest = channel_id
+	@classmethod
+	def get_destination(self):
+		return self.messagedest
+
+def setup(bot):
+	bot.add_cog(messageHandler(bot))
+	print("Message handler loaded.")
