7657485d33d070acd4fb244efe199c7e784000db
igouProto <igouProto@users.noreply.github.com>
2021-02-23 21:54:43 +0800

    nothing changed

--- config.py
+++ config.py
@@ -5,49 +5,54 @@ This module retrieves the essentials for the bot to run, including the token, co
 '''
 version = ''
 
+
 def getToken():
-	try:
-		with open('config.json', 'r') as file:
-			configs = json.load(file)
-			token = configs["token"]
-			print("Token loaded from config.json: {}".format(token))
-	except IOError as e:
-		token = os.environ["TOKEN"]
-		print("Token loaded from os environment variable: {}".format(token))
-	return token
+    try:
+        with open('config.json', 'r') as file:
+            configs = json.load(file)
+            token = configs["token"]
+            print("Token loaded from config.json: {}".format(token))
+    except IOError as e:
+        token = os.environ["TOKEN"]
+        print("Token loaded from os environment variable: {}".format(token))
+    return token
+
 
 def getPrefix():
-	try:
-		with open('config.json', 'r') as file:
-			configs = json.load(file)
-			prefix = configs["prefix"]
-			print("Prefix loaded from config.json: \'{}\'".format(prefix))
-	except IOError as e:
-		prefix = os.environ["PREFIX"]
-		print("Prefix loaded from os environment variable: \'{}\'".format(prefix))
-	return prefix
+    try:
+        with open('config.json', 'r') as file:
+            configs = json.load(file)
+            prefix = configs["prefix"]
+            print("Prefix loaded from config.json: \'{}\'".format(prefix))
+    except IOError as e:
+        prefix = os.environ["PREFIX"]
+        print("Prefix loaded from os environment variable: \'{}\'".format(prefix))
+    return prefix
 
-def getVersion(): # TODO: come to think of it, it is stupid to store version info in config files...
-	return version
+
+def getVersion():  # it was stupid to store version info in config files...
+    return version
+
 
 def getBackstage():
-	try:
-		with open('config.json', 'r') as file:
-			configs = json.load(file)
-			backstage = configs["backstage_id"]
-			print("Backstage channel loaded from config.json: {}".format(backstage))
-	except IOError as e:
-		backstage = os.environ["BACKSTAGE"]
-		print("Version info loaded from os environment variable: {}".format(backstage))
-	return backstage
+    try:
+        with open('config.json', 'r') as file:
+            configs = json.load(file)
+            backstage = configs["backstage_id"]
+            print("Backstage channel loaded from config.json: {}".format(backstage))
+    except IOError as e:
+        backstage = os.environ["BACKSTAGE"]
+        print("Version info loaded from os environment variable: {}".format(backstage))
+    return backstage
+
 
 def getPortal():
-	try:
-		with open('config.json', 'r') as file:
-			configs = json.load(file)
-			portal = configs["portal_id"]
-			print("Portal channel loaded from config.json: {}".format(portal))
-	except IOError as e:
-		portal = os.environ["PORTAL"]
-		print("Portal channel loaded from os environment variable: {}".format(portal))
-	return portal
+    try:
+        with open('config.json', 'r') as file:
+            configs = json.load(file)
+            portal = configs["portal_id"]
+            print("Portal channel loaded from config.json: {}".format(portal))
+    except IOError as e:
+        portal = os.environ["PORTAL"]
+        print("Portal channel loaded from os environment variable: {}".format(portal))
+    return portal
