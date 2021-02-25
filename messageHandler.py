6a731af5ccb58e6d4884404fdfe47b312c252699
igouProto <igouProto@users.noreply.github.com>
2021-02-25 23:56:10 +0800

    lacking keyword file no longer stops booting

--- messageHandler.py
+++ messageHandler.py
@@ -12,13 +12,16 @@ class messageHandler(commands.Cog):
         self.response = {}
         self.portal_id = int(config.getPortal())  # load the portal id from config
         # read the keyword pairs from file
-        with open('keywords.txt', 'r') as data:
-            for line in data:
-                (kw, resp) = line.split(':', 1)
-                self.response[kw] = str(resp)
-            for (kw, resp) in self.response.items():
-                self.keywords.append(kw)
-        data.close()
+        try:
+            with open('keywords.txt', 'r') as data:
+                for line in data:
+                    (kw, resp) = line.split(':', 1)
+                    self.response[kw] = str(resp)
+                for (kw, resp) in self.response.items():
+                    self.keywords.append(kw)
+            data.close()
+        except IOError:
+            print('keyword file does not exist!')
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -30,10 +33,12 @@ class messageHandler(commands.Cog):
                 # print('keyword hit')
                 msg = self.response[message.content]
                 await message.channel.send(msg)
+        '''
         if '呼哈' in message.content and message.author != self.bot.user:
             time = message.created_at + timedelta(hours=1)
             msg = '你的下一次呼哈：{:%H:%M:%S}'.format(time)
             await message.channel.send(msg)
+        '''
     # portal talking function
         if message.channel.id == self.portal_id:
             await self.bot.get_channel(self.messagedest).send(message.content)
