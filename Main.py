4d9da2a9c4b12906e9cf5fbefaacfc3607183725
igouProto <igouProto@users.noreply.github.com>
2021-05-03 23:16:50 +0800

    update ver number

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ This is the main program of the bot, it creates a bot instance, loads all the ex
 print("Booting SUICA...")  # totally unnecessary but makes the booting sequence look cooler UwU
 bot = commands.Bot(command_prefix=config.getPrefix())
 TOKEN = config.getToken()
-config.version = '2.6.2'
+config.version = '2.6.3'
 
 # TODO (igouP): Gather all the extensions and use one single statement to load all of them.
 bot.load_extension('manager')
